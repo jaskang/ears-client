23205a831ca99704a5fe17461a3319a37e510490
JasKang <jaskang@qq.com>
2019-06-28 16:35:56 +0800

    feat:init

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/recommended', '@vue/prettier'],
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+};
